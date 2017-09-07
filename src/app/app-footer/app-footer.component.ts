7fb2a8456e88eb7707dfbf9bab3efb0bf4bcee97
gogokoala <372258@qq.com>
2017-09-07 16:34:47 +0800

    modifiy complex page

--- src/app/app-footer/app-footer.component.ts
+++ src/app/app-footer/app-footer.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app-footer.component.css']
 })
 export class AppFooterComponent implements OnInit {
-  company = '派拓源律所';
+  company = '云知搜信息技术有限公司';
   address = '江苏省无锡市';
   hotline = '4001-185-185';
   icp = '京ICP备11017069号-5';
